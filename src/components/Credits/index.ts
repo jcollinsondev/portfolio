5df99798cdd5ec570155b7e7f04376bd90fb5fd7
Julian Collinson <julian.collinson1998@gmail.com>
2023-03-14 13:56:59 +0100

    improved layout

---
+++ src/components/Credits/index.ts
@@ -0,0 +1,2 @@
+import Credits from './Credits.astro';
+export default Credits;
--- src/components/Work/project.interface.ts
+++ src/components/Work/project.interface.ts
@@ -2,8 +2,9 @@ export default interface IProject {
   name: string;
   description: string;
   links: {
-    website: string;
-    github: string;
+    website?: string;
+    github?: string;
+    figma?: string;
   };
   stack: string[];
 }
